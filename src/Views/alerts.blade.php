eba7f3310a264340f83556a4899ef5ebe393a118
Oliver Kaufmann <okaufmann91@gmail.com>
2016-01-05 07:47:59 +0100

    Do not escape text due to be able to use special chars and formatting

--- src/Views/alerts.blade.php
+++ src/Views/alerts.blade.php
@@ -1,8 +1,8 @@
 @if (session()->has('flash_message'))
     <script>
         swal({
-            title: "{{ session('flash_message.title') }}",
-            text:  "{{ session('flash_message.message') }}",
+            title: "{!! session('flash_message.title') !!}",
+            text:  "{!! session('flash_message.message') !!}",
             type: "{{ session('flash_message.type') }}",
             timer: 2500,
             showConfirmButton: false
