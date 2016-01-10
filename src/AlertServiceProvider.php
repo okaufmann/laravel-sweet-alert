5227fa72a5f52bb30442d8e82123a76ba2b93d82
Muhammad Zamroni <halo@matriphe.com>
2016-01-10 11:22:52 +0700

    Add publish view mechanism on vendor:publish
    It will copy the alerts.blade.php to resources/views/vendor/SweetAlert so we can easily modify the view

--- src/AlertServiceProvider.php
+++ src/AlertServiceProvider.php
@@ -35,6 +35,7 @@ class AlertServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/Libraries/sweetalert.css' => public_path('css/sweetalert.css'),
             __DIR__.'/Libraries/sweetalert.js' => public_path('js/sweetalert.js'),
+            __DIR__.'/Views/alerts.blade.php' => base_path('resources/views/vendor/SweetAlert'),
         ], 'alerts');
     }
 }
